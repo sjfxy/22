3373b34b40c13d9fa9fecc48a4c98445e87f65f0
Shijun <986244259@qq.com>
2021-08-18 13:08:09 +0800

    携程任务处理

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -107,6 +107,8 @@ class IndexController extends Controller
         $pa = new Parallel(5);
         $info = $this->getPar($pa);
 
+        //迁移处理的方式
+
 
         //因为只有面向对象 这样才可以 在我们 model value service
         //内部的属于
